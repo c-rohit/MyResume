4e596b39bbcc333a6d3a44be53f07ba92b2ea912
Rohit C <rcvium24@gmail.com>
2024-07-21 16:28:20 +0530

    Uploaded files

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <title>Rohit C丨Resume</title>
+      <embed className="resume" src="/documents/resume.pdf" type="application/pdf"></embed>
+    </>
+  );
+}
